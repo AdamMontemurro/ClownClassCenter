7df06c51d823e5cab3608d577d6bc84802f0e33a
adam <adammontemurro@gmail.com>
2023-03-16 16:27:16 -0400

    added more courses to seeder

--- seeders/20230316191718-classes.js
+++ seeders/20230316191718-classes.js
@@ -2,12 +2,12 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     let courses = [
       {
-      name: 'Juggling',
-      course_code: 'Jug101',
-      createdAt: new Date(),
+        name: 'Juggling',
+        course_code: 'Jug101',
+        createdAt: new Date(),
         updatedAt: new Date()
       },
       {
@@ -15,27 +15,27 @@ module.exports = {
         course_code: 'Jug201',
         createdAt: new Date(),
         updatedAt: new Date()
-        },
+      },
       {
-         name: 'Advanced Juggling',
-         course_code: 'Jug301',
-         createdAt: new Date(),
+        name: 'Advanced Juggling',
+        course_code: 'Jug301',
+        createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        name: 'Clown Makup',
+        name: 'Clown Makeup',
         course_code: 'MU101',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        name: 'Intermediate Clown Makup',
+        name: 'Intermediate Clown Makeup',
         course_code: 'MU201',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        name: 'Advanced Clown Makup',
+        name: 'Advanced Clown Makeup',
         course_code: 'MU301',
         createdAt: new Date(),
         updatedAt: new Date()
@@ -59,17 +59,73 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-          name: 'Pie Throwing',
-          course_code: 'PT101',
-          createdAt: new Date(),
+        name: 'Pie Throwing',
+        course_code: 'PT101',
+        createdAt: new Date(),
         updatedAt: new Date()
-      }  
-    ];
-     await queryInterface.bulkInsert('courses', courses)
+      },
 
+    ];
+
+    const clownSchoolCourses = [
+      {
+        name: "Clown Fundamentals", code: "CF101",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Physical Comedy", code: "PC201",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Juggling", code: "JG101",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Balloon Twisting", code: "BT301",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Slapstick Comedy", code: "SC401",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Clown Make-Up", code: "MU201",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Improvisation", code: "IV301",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Prop Building", code: "PB101",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Silly Songs", code: "SS401",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Character Development", code: "CD201",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ];
+
+
+    await queryInterface.bulkInsert('courses', courses)
+    await queryInterface.bulkInsert('courses', clownSchoolCourses )
   },
 
-  async down (queryInterface, Sequelize) {
-   await queryInterface.bulkDelete('courses')
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('courses')
   }
 };
