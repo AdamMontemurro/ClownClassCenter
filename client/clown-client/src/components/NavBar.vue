2f812160068df2e35e427530e34b8e5dc69fff15
adam <adammontemurro@gmail.com>
2023-03-16 13:57:53 -0400

    styled navbar

--- client/clown-client/src/components/NavBar.vue
+++ client/clown-client/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav id="navigation">
     <router-link to="/">Home</router-link>
     <router-link to="/students">Students</router-link>
     <router-link to="/courses">Courses</router-link>
@@ -13,5 +13,22 @@
 </script>
 
 <style scoped>
+#navigation {
+  display: flex;
+  background-color: #780116;
+  width:100%;
+  height: 80px;
+  
+}
+nav a {
+  margin-top: 1em;
+  margin-right: 1em;
+  margin-left: 2em;
+  color: #d8572A;
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 22px;
+  display: flex;
+}
 
 </style>
