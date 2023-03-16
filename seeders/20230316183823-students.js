0b5ec3f90f49ad847e2fbe4ae973359025b4996a
Willard Szeto <wszeto11@gmail.com>
2023-03-16 16:20:43 -0400

    to pull from origin main

--- seeders/20230316183823-students.js
+++ seeders/20230316183823-students.js
@@ -1,30 +1,24 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
+  async up(queryInterface, Sequelize) {
     let students = [
       {
-        id: 1,
         first_name: 'John',
         last_name: 'Doe',
         email: 'johndoe@example.com',
         createdAt: new Date(),
         updatedAt: new Date()
-        
       },
       {
-        id: 2,
         first_name: 'Jane',
         last_name: 'Doe',
         email: 'janedoe@example.com',
         createdAt: new Date(),
         updatedAt: new Date()
-
       },
       {
-        id: 3,
         first_name: 'Bob',
         last_name: 'Smith',
         email: 'bobsmith@example.com',
@@ -32,7 +26,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 4,
         first_name: 'Samantha',
         last_name: 'Jones',
         email: 'samanthajones@example.com',
@@ -40,7 +33,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 5,
         first_name: 'Michael',
         last_name: 'Johnson',
         email: 'michaeljohnson@example.com',
@@ -48,7 +40,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 6,
         first_name: 'Emily',
         last_name: 'Davis',
         email: 'emilydavis@example.com',
@@ -56,7 +47,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 7,
         first_name: 'David',
         last_name: 'Wilson',
         email: 'davidwilson@example.com',
@@ -64,7 +54,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 8,
         first_name: 'Sarah',
         last_name: 'Brown',
         email: 'sarahbrown@example.com',
@@ -72,7 +61,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 9,
         first_name: 'Christopher',
         last_name: 'Garcia',
         email: 'christophergarcia@example.com',
@@ -80,20 +68,18 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 10,
         first_name: 'Jessica',
         last_name: 'Rodriguez',
         email: 'jessicarodriguez@example.com',
         createdAt: new Date(),
         updatedAt: new Date()
       }
-    ];
+    ]
 
     await queryInterface.bulkInsert('students', students)
-
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('students')
   }
-};
+}
