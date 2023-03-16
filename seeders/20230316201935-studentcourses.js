38a5962b662980e0e06141bbc62e239ea755b790
Willard Szeto <wszeto11@gmail.com>
2023-03-16 17:06:12 -0400

    committing changes.....

--- seeders/20230316201935-studentcourses.js
+++ seeders/20230316201935-studentcourses.js
@@ -1,26 +1,27 @@
-'use strict';
-const { Student, Course, sequelize } = require('../models');
+'use strict'
+const { Student, Course, sequelize } = require('../models')
+const falso = require('@ngneat/falso')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const studentcourses = await Promise.all(
       [...Array(60)].map(async () => {
-        const student = await Student.findOne({ order: sequelize.random() });
-        const course = await Course.findOne({ order: sequelize.random() });
+        const student = await Student.findOne({ order: sequelize.random() })
+        const course = await Course.findOne({ order: sequelize.random() })
         return {
           student_id: student.id,
           course_id: course.id,
           grade: falso.randNumber({ max: 4 }),
           createdAt: new Date(),
-          updatedAt: new Date(),
-        };
+          updatedAt: new Date()
+        }
       })
-    );
-    return queryInterface.bulkInsert('studentscourses', studentcourses, {});
+    )
+    return queryInterface.bulkInsert('studentcourses', studentcourses, {})
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('students_courses', null, {});
-  },
-};
+    await queryInterface.bulkDelete('studentcourses', null, {})
+  }
+}
