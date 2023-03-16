f76a38365e16002177c41690ed725ec3ccf2f73c
adam <adammontemurro@gmail.com>
2023-03-16 14:40:17 -0400

    student seeder created

--- client/clown-client/src/pages/StudentsList.vue
+++ client/clown-client/src/pages/StudentsList.vue
@@ -2,8 +2,11 @@
   <div>
     <h1 class="pageTitle">Students</h1>
     <img id="logoImg" src="https://i.pinimg.com/736x/30/24/e8/3024e8e03debed075156a58ccd9cfd22.jpg" alt="">
-    <div id="studentContainer">
-      
+    <div id="studentContainer" v-for="student in students" :key="student.id">
+      <div class="studentCard">
+        <h4>{{ student.first_name }}</h4>
+        <h4>{{ student.last_name  }}</h4>
+      </div>
     </div>
   </div>
 </template>
