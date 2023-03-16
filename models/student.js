4e2d709c202bdd6c19c2ad7cdd8a903e97da9177
Willard Szeto <wszeto11@gmail.com>
2023-03-16 11:43:14 -0400

    Created models for student, studentcourse, user, and course

---
+++ models/student.js
@@ -0,0 +1,44 @@
+'use strict'
+const { Model, Sequelize } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    static associate(models) {
+      Student.belongsToMany(models.Course),
+        {
+          through: models.StudentCourse,
+          as: 'courses',
+          foreignKey: 'student_id'
+        }
+    }
+  }
+  Student.init(
+    {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true //needed?
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Student',
+      tableName: 'students'
+    }
+  )
+  return Student
+}
+
+// student hasMany courses
+// fk
