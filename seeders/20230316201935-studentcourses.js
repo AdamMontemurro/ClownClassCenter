d3afb3dbc9992060caf07789759790ddba3050d4
jcurti2 <joshua.curtis24@gmail.com>
2023-03-17 07:15:14 -0700

    seeded and migrated

--- seeders/20230316201935-studentcourses.js
+++ seeders/20230316201935-studentcourses.js
@@ -18,10 +18,10 @@ module.exports = {
         };
       })
     );
-    return queryInterface.bulkInsert('studentcourses', studentcourses, {});
+    return queryInterface.bulkInsert('students_courses', studentcourses, {});
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('studentcourses', null, {});
+    await queryInterface.bulkDelete('students_courses', null, {});
   },
 };
